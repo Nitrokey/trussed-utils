d09b79c63e301cb3bdeb193935dfd0e948686c96
Sosthène Guédon <sosthene@nitrokey.com>
2023-02-21 14:48:42 +0100

    Add encryption for serde data

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 #![no_std]
 
+use serde::{de::DeserializeOwned, Serialize};
 use trussed::{
+    config::MAX_MESSAGE_LENGTH,
     try_syscall,
     types::{KeyId, Location, Mechanism, Message, PathBuf},
-    Client, Error,
+    Bytes, Client, Error,
 };
 
 #[cfg(not(feature = "delog"))]
@@ -15,6 +17,7 @@ delog::generate_macros!();
 
 const CHACHA_NONCE_LEN: usize = 12;
 const POLY1305_TAG_LEN: usize = 16;
+const MAX_ENCRYPTED_DATA_LEN: usize = MAX_MESSAGE_LENGTH - CHACHA_NONCE_LEN - POLY1305_TAG_LEN;
 
 pub trait EncryptionUtil: Client + Sized {
     /// Stores data encrypted with ChaCha8Poly1305
@@ -80,6 +83,34 @@ pub trait EncryptionUtil: Client + Sized {
             Error::AeadError
         })?;
         Ok(plaintext)
+    }
+
+    fn store_encrypted_struct<T: Serialize>(
+        &mut self,
+        data: &T,
+        path: PathBuf,
+        location: Location,
+        encryption_key: KeyId,
+    ) -> Result<(), Error> {
+        let bytes: Bytes<MAX_ENCRYPTED_DATA_LEN> =
+            trussed::cbor_serialize_bytes(data).map_err(|_err| {
+                error!("Failed to serialize data: {:?}", _err);
+                Error::CborError
+            })?;
+        self.store_encrypted(&bytes, path, location, encryption_key)
+    }
+
+    fn load_encrypted_struct<T: DeserializeOwned>(
+        &mut self,
+        path: PathBuf,
+        location: Location,
+        encryption_key: KeyId,
+    ) -> Result<T, Error> {
+        let data = self.load_encrypted(path, location, encryption_key)?;
+        trussed::cbor_deserialize(&data).map_err(|_err| {
+            error!("Failed to deserialize data: {:?}", _err);
+            Error::CborError
+        })
     }
 }
 
